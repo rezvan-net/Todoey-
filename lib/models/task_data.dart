86818e84f109be875a609bfc43dba814f9ff123c
Shaghayegh Rahmani <74653444+rezvan-net@users.noreply.github.com>
2021-01-11 22:30:06 +0330

    Update task_data.dart

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/foundation.dart';
 import 'package:todoey_flutter/models/task.dart';
 import 'dart:collection';
+import 'task.dart';
 
-class TaskData extends ChangeNotifier {
+
+class TaskData extends ChangeNotifier{
   List<Task> _tasks = [
-    Task(name: 'Buy milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
+    Task(name: 'buy milk'),
+    Task(name: 'buy bread'),
+    Task(name: 'buy soap'),
   ];
 
-  UnmodifiableListView<Task> get tasks {
+  UnmodifiableListView <Task> get tasks {
     return UnmodifiableListView(_tasks);
   }
 
-  int get taskCount {
+
+  int get taskCount{
     return _tasks.length;
   }
 
-  void addTask(String newTaskTitle) {
-    final task = Task(name: newTaskTitle);
-    _tasks.add(task);
-    notifyListeners();
+  void addTask (String  newTextTitle){
+    // tasks.add(Task(name:'xgbnmmhg'));
+final task= Task (name: newTextTitle);
+_tasks.add(task);
+notifyListeners();
   }
 
-  void updateTask(Task task) {
-    task.toggleDone();
-    notifyListeners();
+
+void updateTask (Task task){
+task.toggleDone();
+notifyListeners();
   }
 
-  void deleteTask(Task task) {
+
+void deleteTask (Task task){
     _tasks.remove(task);
-    notifyListeners();
-  }
+notifyListeners();
+}
+
+
 }
